f0c1171214f0eea24286b5cce52d288d848be819
Jacek Jackowiak <jacek1986@gmail.com>
2017-11-08 19:46:42 +0100

    IU-2017.2.5 <Jacek@Lenovo-PC Create baseRefactoring.xml, _@user_Material Theme - Default.icls, markdown.xml, IntelliLang.xml, ide.general.xml, git.xml, _@user_Material Theme - Darker.icls, project.default.xml, diff.xml, equalsHashCodeTemplates.xml, other.xml, Default.xml, JUnit4 Test Method.java, file.template.settings.xml, code.style.schemes, find.xml, ui.lnf.xml, File Header.java, lombok.xml, filetypes.xml, keymap.xml, remote-servers.xml, gradle.run.settings.xml, remote-server-client-libraries.xml, colors.scheme.xml, Darcula copy.icls, databaseDrivers.xml, github_settings.xml, mavenVersion.xml, Material_Peacock.icls, bootstrap.xml, Default.xml, laf.xml, jdk.table.xml, debugger.renderers.xml, path.macros.xml, JUnit4 Test Class.java, web-browsers.xml, material_theme.xml, vcs.xml, editor.codeinsight.xml, databaseSettings.xml, toStringTemplates.xml, editor.xml, usageView.xml, debugger.xml

---
+++ fileTemplates/code/JUnit4 Test Class.java
@@ -0,0 +1,5 @@
+import static org.assertj.core.api.Assertions.assertThat;
+#parse("File Header.java")
+public class ${NAME} {
+  ${BODY}
+}
---
+++ fileTemplates/code/JUnit4 Test Method.java
@@ -0,0 +1,10 @@
+@org.junit.Test
+public void should${NAME}() {
+  // given
+  
+  // when
+  ${BODY}
+  
+  // then
+  
+}
